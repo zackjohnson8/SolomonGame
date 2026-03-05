0d9ba4fb8cc4be3f9a1e3f84bab287e8904ff0dc
zackjohnson8 <zackjohnson8@gmail.com>
2026-03-04 17:06:42 -0700

    Add a main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+
+if __name__ == "__main__":
+    name = "Hello Solomon, We'll be using pygame for this project."
+    name = name.replace("S", "F")
+    numberOfTimes = 10
+    for i in range(10):
+        print(f"{name}\n")
